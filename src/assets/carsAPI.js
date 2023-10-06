cf2f246bc39b314312e88cf874cdd460cdd134e6
¨[Serghio07]¨ <[ticona465@gmail.com]>
2023-10-06 11:27:07 -0400

    subiendo

---
+++ src/assets/carsAPI.js
@@ -0,0 +1,84 @@
+
+export const cars = [
+  {
+    title: "Sedans",
+    description:"Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    imgUrl: "/icon-sedans.svg",
+    theme: "orange",
+  },
+  {
+    title: "Suvs",
+    description:"Take an SUV for its spacious interior, power and veraitily. Perfect for your next faminily vacation and off-road adventures.",
+    imgUrl: "/icon-suvs.svg",
+    theme: "green",
+  },
+  {
+    title: "Luxury",
+    description:"Cruise in the best car brands without the bloaoated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+    imgUrl: "/icon-luxury.svg",
+    theme: "dark-green",
+  },
+  {
+    title: "Toyota Camry",
+    description:"The Toyota Camry is a midsize sedan known for its reliability and fuel efficiency. It offers a spacious interior and comfortable driving experience.",
+    imgUrl:"/1.png",
+    theme: "red",
+  },
+  {
+    title: "Ford Mustang",
+    description:"The Ford Mustang is an iconic American muscle car that combines power and style. It has a powerful engine and a sporty, elegant design.",
+    imgUrl:"/2.png",
+    theme: "violet",
+  },
+  {
+    title:"Honda Civic",
+    description:"The Honda Civic is a popular compact car known for its fuel efficiency, reliability, and agile handling.",
+    imgUrl:"/3.png",
+    theme: "pink",
+  },
+  {
+    title:"Jeep Wrangler",
+    description:"The Jeep Wrangler is an iconic off-road SUV famous for its off-road capability and rugged design. It's ideal for adventure enthusiasts.",
+    imgUrl:"/4.png",
+    theme:"gray"
+  },
+  {
+    title:"Volkswagen Golf",
+    description:" The Volkswagen Golf is a popular European compact known for its build quality and smooth handling. 0",
+    imgUrl:"/5.png",
+    theme:"salmon",
+  },
+  {
+    title:"Tesla Model 3",
+    description:" The Tesla Model 3 is a luxury electric car known .",
+    imgUrl:"/6.png",
+    theme:"yelow",
+  },
+  {
+    title:"Chevrolet Silverado",
+    description:"The Chevrolet Silverado is a full-size pickup truck popular in the United States for its towing and hauling capability",
+    imgUrl:"/7.png",
+    theme:"green",
+  },
+  {
+
+    title:"BMW 3 Series",
+    description:"The BMW 3 Series is a luxury sedan known for its sporty handling and high-quality interior. It's a popular choice in the premium car segment.",
+    imgUrl:"/8.png",
+    theme:"brown",
+  },
+  {
+    title:"Nissan Rogue",
+    description:" The Nissan Rogue is a compact SUV that offers a balance of interior space, efficiency, and safety features. It's popular among families.",
+    imgUrl:"/9.png",
+    theme:"gold",
+  },
+  {
+    title:"Audi Q5",
+    description:"The Audi Q5 is a luxury SUV that combines a luxurious interior with solid performance. It offers advanced technology.",
+    imgUrl:"/coche.png",
+    theme:"lemegreen",
+
+
+  }
+]
